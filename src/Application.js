4807339701965c561a2275cc98b24188700def1f
Jickson P <jicksonstephen@gmail.com>
2016-10-16 18:02:36 +0530

    Single entry point to ios and android

---
+++ src/Application.js
@@ -0,0 +1,35 @@
+//@flow
+
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+});
+
+export default class Application extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native Jickson!
+                </Text>
+            </View>
+        );
+    }
+}
